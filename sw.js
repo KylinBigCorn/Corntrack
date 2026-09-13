f58e507fd6247f3e6173b80a947da77597358414
Du YiMing <429984200@qq.com>
2026-09-13 22:56:47 +0800

    Cache Corntrack V4 split scripts

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='corntrack-v4-20260913';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='corntrack-v4-20260913b';
+const ASSETS=['./','./index.html','./styles.css','./app-v4-1.js','./app-v4-2a.js','./app-v4-2b.js','./app-v4-3a.js','./app-v4-3b.js','./app-v4-4.js','./app-v4-5.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
